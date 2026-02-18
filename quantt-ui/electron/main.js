d4f6d5c998e11742d7182db60806b6c8dcff9304
THI100 <thiagocas100@gmail.com>
2026-02-17 21:07:46 -0300

    Working on ui

--- quantt-ui/electron/main.js
+++ quantt-ui/electron/main.js
@@ -2,8 +2,8 @@ import { app, BrowserWindow } from "electron";
 
 function createWindow() {
   const win = new BrowserWindow({
-    width: 1200,
-    height: 800
+    width: 1280,
+    height: 720
   });
 
   win.loadURL("http://localhost:5173");
