a339761577ba68e47e7a13a54b238a2a296e1482
THI100 <thiagocas100@gmail.com>
2026-02-25 19:57:52 -0300

    Start for SQL persistance.

---
+++ quantt-engine/persistance/connection.py
@@ -0,0 +1,14 @@
+from sqlalchemy import create_all, create_engine
+from sqlalchemy.orm import DeclarativeBase, sessionmaker
+
+DB_URL = "sqlite:///./my_database.db"
+
+# Create the Engine
+engine = create_engine(DB_URL, connect_args={"check_same_thread": False})
+
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+
+# Base class for models to inherit from
+class Base(DeclarativeBase):
+    pass
---
+++ quantt-engine/persistance/models.py
@@ -0,0 +1,11 @@
+from sqlalchemy.orm import Mapped, mapped_column
+
+from .connection import Base
+
+
+class User(Base):
+    __tablename__ = "users"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    username: Mapped[str] = mapped_column(unique=True, nullable=False)
+    email: Mapped[str] = mapped_column(nullable=True)
