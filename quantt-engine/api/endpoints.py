268c9ea1e3e04ff53f773722a890a7212f718b57
THI100 <thiagocas100@gmail.com>
2026-03-21 20:37:42 -0300

    Possibly working

--- quantt-engine/api/endpoints.py
+++ quantt-engine/api/endpoints.py
@@ -3,6 +3,7 @@ import threading
 from fastapi import APIRouter
 
 from core.bot import TradingBot
+from execution.position_manager import manage_open_symbols as mos
 
 router = APIRouter()
 bot = TradingBot()
@@ -19,10 +20,28 @@ def start_trigger():
     bot_thread = threading.Thread(target=bot.start, daemon=True)
     bot_thread.start()
 
-    return {"status": "started"}
+    return {"status": "activating"}
+
+
+@router.post("/bot/stop")
+def stop_trigger():
+    global bot_thread
+    if bot.is_running == False:
+        return {"status": "deactivated"}
+
+    bot_thread = threading.Thread(target=bot.stop, daemon=True)
+    bot_thread.stop()
+
+    return {"status": "deactivating"}
 
 
 @router.get("/bot/status")
 def get_status():
-    # This returns immediately, so it doesn't block anything
-    return {"is_running": bot.is_running}
+    global bot_thread
+    if bot.is_running == False:
+        return {"status": "deactivated"}
+
+    bot_thread = threading.Thread(target=mos, deamon=True)
+    markets = bot_thread.mos()
+    # This returns the open markets and etc, it is a dict, equivalent of a object for JS.
+    return markets, {"status": "active"}
