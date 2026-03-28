32403af1cffdeb0127aad1b807b657a62cd55ae3
THI100 <thiagocas100@gmail.com>
2026-03-27 23:43:33 -0300

    .

---
+++ quantt-engine/reporting_portfolio/roll.py
@@ -0,0 +1,42 @@
+# --- PnL and equitity curve --- #
+
+
+def equity_curve():
+    a = 0
+    #
+    #
+    return a
+
+
+def seasonal_pnl():
+    a = 0
+    #
+    #
+    return a
+
+
+# --- Risk and Drawdown ---#
+
+
+def drawdown_series():
+    a = 0
+    #
+    #
+    return a
+
+
+# --- Exposure and Activity --- #
+
+
+def daily_trade_count():
+    a = 0
+    #
+    #
+    return a
+
+
+def exposure_over_time():
+    a = 0
+    #
+    #
+    return a
---
+++ quantt-engine/reporting_portfolio/static.py
@@ -0,0 +1,35 @@
+# --- Risk and Drawdown ---#
+
+
+def max_drawdown():
+    a = 0
+    #
+    #
+    return a
+
+
+def sharpe_ratio():
+    a = 0
+    #
+    #
+    return a
+
+
+# --- Trade Quality --- #
+
+
+def avg_hold_time():
+    a = 0
+    #
+    #
+    return a
+
+
+# --- Exposure and Activity --- #
+
+
+def best_and_worst_trades():
+    a = 0
+    #
+    #
+    return a
