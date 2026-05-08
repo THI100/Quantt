a3806c43f0cb485a81fc152bd8b80251364cb8a9
THI100 <thiagocas100@gmail.com>
2026-05-07 23:51:07 -0300

    Hard error handling.

--- quantt-engine/exchange/selector.py
+++ quantt-engine/exchange/selector.py
@@ -1,6 +1,19 @@
 from loguru import logger
 
 from config import settings
+from exchange.awm import ENV_PATH, ensure_env_file
+
+logger.info(ensure_env_file())
+
+file_path = ENV_PATH
+
+if file_path.stat().st_size == 0:
+    logger.error(
+        "Oops, it seems you dont have data on your .env, write directly on it or use the API page to modify the values."
+    )
+    raise Exception(
+        "your .env file doesn´t have the values needed, write directly on it or use the API page to modify the values."
+    )
 
 
 def get_exchange_client(exchange_name: str = settings.watcher.get_config().exchange):
