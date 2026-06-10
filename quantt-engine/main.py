14e640c9389dbd2f95f48210a0622859ea96d559
THI100 <thiagocas100@gmail.com>
2026-06-10 19:56:06 -0300

    Small aditions (uvicorn logging and its configuration, CORS activation for 4173 and 5173) for final production 0.1 version launch.

--- quantt-engine/main.py
+++ quantt-engine/main.py
@@ -4,6 +4,7 @@ from fastapi.middleware.cors import CORSMiddleware
 
 from api import principal, report, set
 from utils import log
+from utils.log import LOGGING_CONFIG
 from utils.stream_manager import log_stream
 
 app = FastAPI(title="quantt_engine")
@@ -11,6 +12,8 @@ app = FastAPI(title="quantt_engine")
 origins = [
     "http://localhost:5173",
     "http://127.0.0.1:5173",
+    "http://127.0.0.1:4173",
+    "http://localhost:4173",
 ]
 
 app.add_middleware(
@@ -29,4 +32,4 @@ app.include_router(set.s_route)
 if __name__ == "__main__":
     log.setup_logging(stream_callback=log_stream.broadcast)
     # Run the server
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=8000, log_config=LOGGING_CONFIG)
--- quantt-engine/utils/log.py
+++ quantt-engine/utils/log.py
@@ -41,3 +41,49 @@ def setup_logging(stream_callback=None):
             stream_callback(payload)
 
         logger.add(stream_sink, level="INFO")
+
+
+# ------------------------ Special for Uvicorn on Production ------------------------ #
+
+LOGGING_CONFIG = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "formatters": {
+        "default": {
+            "format": "%(levelprefix)s %(message)s",
+            "()": "uvicorn.logging.DefaultFormatter",
+            "use_colors": False,
+        },
+        "access": {
+            "format": '%(levelprefix)s %(client_addr)s - "%(request_line)s" %(status_code)s',
+            "()": "uvicorn.logging.AccessFormatter",
+            "use_colors": False,
+        },
+    },
+    "handlers": {
+        "default": {
+            "formatter": "default",
+            "class": "logging.StreamHandler",
+            "stream": "ext://sys.stdout",
+        },
+        "access": {
+            "formatter": "access",
+            "class": "logging.StreamHandler",
+            "stream": "ext://sys.stdout",
+        },
+    },
+    "loggers": {
+        "uvicorn": {
+            "handlers": ["default"],
+            "level": "INFO",
+        },
+        "uvicorn.error": {
+            "level": "INFO",
+        },
+        "uvicorn.access": {
+            "handlers": ["access"],
+            "level": "INFO",
+            "propagate": False,
+        },
+    },
+}
